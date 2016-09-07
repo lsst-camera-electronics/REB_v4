3386b6286f6bfe419f8b92e0823601d0d401165a
Stefano Russo <srusso@slac.stanford.edu>
2016-09-07 11:18:48 -0700

    first commit

---
+++ src/ipcore_dir/mon_xadc/example_design/mon_xadc_exdes.vhd
@@ -0,0 +1,136 @@
+-- file: mon_xadc_exdes.vhd
+-- (c) Copyright 2009 - 2011 Xilinx, Inc. All rights reserved.
+-- 
+-- This file contains confidential and proprietary information
+-- of Xilinx, Inc. and is protected under U.S. and
+-- international copyright and other intellectual property
+-- laws.
+-- 
+-- DISCLAIMER
+-- This disclaimer is not a license and does not grant any
+-- rights to the materials distributed herewith. Except as
+-- otherwise provided in a valid license issued to you by
+-- Xilinx, and to the maximum extent permitted by applicable
+-- law: (1) THESE MATERIALS ARE MADE AVAILABLE "AS IS" AND
+-- WITH ALL FAULTS, AND XILINX HEREBY DISCLAIMS ALL WARRANTIES
+-- AND CONDITIONS, EXPRESS, IMPLIED, OR STATUTORY, INCLUDING
+-- BUT NOT LIMITED TO WARRANTIES OF MERCHANTABILITY, NON-
+-- INFRINGEMENT, OR FITNESS FOR ANY PARTICULAR PURPOSE; and
+-- (2) Xilinx shall not be liable (whether in contract or tort,
+-- including negligence, or under any other theory of
+-- liability) for any loss or damage of any kind or nature
+-- related to, arising under or in connection with these
+-- materials, including for any direct, or any indirect,
+-- special, incidental, or consequential loss or damage
+-- (including loss of data, profits, goodwill, or any type of
+-- loss or damage suffered as a result of any action brought
+-- by a third party) even if such damage or loss was
+-- reasonably foreseeable or Xilinx had been advised of the
+-- possibility of the same.
+-- 
+-- CRITICAL APPLICATIONS
+-- Xilinx products are not designed or intended to be fail-
+-- safe, or for use in any application requiring fail-safe
+-- performance, such as life-support or safety devices or
+-- systems, Class III medical devices, nuclear facilities,
+-- applications related to the deployment of airbags, or any
+-- other applications that could lead to death, personal
+-- injury, or severe property or environmental damage
+-- (individually and collectively, "Critical
+-- Applications"). Customer assumes the sole risk and
+-- liability of any use of Xilinx products in Critical
+-- Applications, subject only to applicable laws and
+-- regulations governing limitations on product liability.
+-- 
+-- THIS COPYRIGHT NOTICE AND DISCLAIMER MUST BE RETAINED AS
+-- PART OF THIS FILE AT ALL TIMES.
+
+------------------------------------------------------------------------------
+-- XADC wizard example design
+------------------------------------------------------------------------------
+-- This example design instantiates the generated file containing the
+-- System Monitor instantiation.
+------------------------------------------------------------------------------
+
+
+library ieee;
+use ieee.std_logic_1164.all;
+use ieee.numeric_std.all;
+Library UNISIM;
+use UNISIM.VCOMPONENTS.ALL;
+
+entity mon_xadc_exdes is
+    port (
+          DADDR_IN            : in  STD_LOGIC_VECTOR (6 downto 0);
+          DCLK_IN             : in  STD_LOGIC;
+          DEN_IN              : in  STD_LOGIC;
+          DI_IN               : in  STD_LOGIC_VECTOR (15 downto 0);
+          DWE_IN              : in  STD_LOGIC;
+          BUSY_OUT            : out  STD_LOGIC;
+          CHANNEL_OUT         : out  STD_LOGIC_VECTOR (4 downto 0);
+          DO_OUT              : out  STD_LOGIC_VECTOR (15 downto 0);
+          DRDY_OUT            : out  STD_LOGIC;
+          EOC_OUT             : out  STD_LOGIC;
+          EOS_OUT             : out  STD_LOGIC;
+          OT_OUT              : out  STD_LOGIC;
+          VBRAM_ALARM_OUT    : out  STD_LOGIC;
+          VCCAUX_ALARM_OUT    : out  STD_LOGIC;
+          VCCINT_ALARM_OUT    : out  STD_LOGIC;
+          USER_TEMP_ALARM_OUT : out  STD_LOGIC;
+          ALARM_OUT          : out STD_LOGIC;                         -- OR'ed output of all the Alarms
+          VP_IN               : in  STD_LOGIC;
+          VN_IN               : in  STD_LOGIC
+);
+end mon_xadc_exdes;
+
+architecture xilinx of mon_xadc_exdes is
+
+component mon_xadc
+    port (
+          DADDR_IN            : in  STD_LOGIC_VECTOR (6 downto 0);     -- Address bus for the dynamic reconfiguration port
+          DCLK_IN             : in  STD_LOGIC;                         -- Clock input for the dynamic reconfiguration port
+          DEN_IN              : in  STD_LOGIC;                         -- Enable Signal for the dynamic reconfiguration port
+          DI_IN               : in  STD_LOGIC_VECTOR (15 downto 0);    -- Input data bus for the dynamic reconfiguration port
+          DWE_IN              : in  STD_LOGIC;                         -- Write Enable for the dynamic reconfiguration port
+          BUSY_OUT            : out  STD_LOGIC;                        -- ADC Busy signal
+          CHANNEL_OUT         : out  STD_LOGIC_VECTOR (4 downto 0);    -- Channel Selection Outputs
+          DO_OUT              : out  STD_LOGIC_VECTOR (15 downto 0);   -- Output data bus for dynamic reconfiguration port
+          DRDY_OUT            : out  STD_LOGIC;                        -- Data ready signal for the dynamic reconfiguration port
+          EOC_OUT             : out  STD_LOGIC;                        -- End of Conversion Signal
+          EOS_OUT             : out  STD_LOGIC;                        -- End of Sequence Signal
+          OT_OUT              : out  STD_LOGIC;                        -- Over-Temperature alarm output
+          VCCAUX_ALARM_OUT    : out  STD_LOGIC;                        -- VCCAUX-sensor alarm output
+          VCCINT_ALARM_OUT    : out  STD_LOGIC;                        -- VCCINT-sensor alarm output
+          USER_TEMP_ALARM_OUT : out  STD_LOGIC;                        -- Temperature-sensor alarm output
+          VBRAM_ALARM_OUT    : out  STD_LOGIC;                        -- VCCINT-sensor alarm output
+          ALARM_OUT          : out STD_LOGIC;                         -- OR'ed output of all the Alarms
+          VP_IN               : in  STD_LOGIC;                         -- Dedicated Analog Input Pair
+          VN_IN               : in  STD_LOGIC
+);
+end component;
+
+begin
+
+ xadc_wiz_inst : mon_xadc
+port map (
+      DADDR_IN(6 downto 0)    => DADDR_IN(6 downto 0),
+      DCLK_IN                 => DCLK_IN,
+      DEN_IN                  => DEN_IN,
+      DI_IN(15 downto 0)      => DI_IN(15 downto 0),
+      DWE_IN                  => DWE_IN,
+      VBRAM_ALARM_OUT        => VBRAM_ALARM_OUT,
+      VCCAUX_ALARM_OUT        => VCCAUX_ALARM_OUT,
+      VCCINT_ALARM_OUT        => VCCINT_ALARM_OUT,
+      USER_TEMP_ALARM_OUT     => USER_TEMP_ALARM_OUT,
+      BUSY_OUT                => BUSY_OUT,
+      CHANNEL_OUT(4 downto 0) => CHANNEL_OUT(4 downto 0),
+      DO_OUT(15 downto 0)     => DO_OUT(15 downto 0),
+      DRDY_OUT                => DRDY_OUT,
+      EOC_OUT                 => EOC_OUT,
+      EOS_OUT                 => EOS_OUT,
+      OT_OUT                  => OT_OUT,
+      ALARM_OUT          => ALARM_OUT,                         -- OR'ed output of all the Alarms
+      VP_IN                   => VP_IN,
+      VN_IN                   => VN_IN
+         );
+end xilinx;
